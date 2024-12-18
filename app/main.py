752debd5d08a33f5915be34b75d6d01aeae7938a
css186 <cssrm31@gmail.com>
2024-12-18 01:14:11 -0500

    add docker file

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from ..middlewares.logging_middleware import log_requests
 from .services.user_service import router as user_router
 from .services.auth import router as auth_router
+
 # Create a FastAPI instance
 app = FastAPI()
 
@@ -12,7 +13,7 @@ app.middleware("http")(log_requests)
 app.include_router(user_router)
 app.include_router(auth_router)
 
+
 @app.get("/")
 async def index() -> dict:
     return {"message": "Welcome to Foodie-Safety!"}
-
